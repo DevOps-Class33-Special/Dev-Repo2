b38c15d3ee617de53267b552ef8a4419cadf2ba5
Kitzito <mnkitzito@gmail.com>
2023-08-17 05:15:36 +0100

    script.sh added

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#name: engr. kz
+#date: 16/08/23
+#this is a test shell script, implementing commandline arguments
+
+name=$1
+group_name=$2
+
+echo i go by the name $name
+echo i am a member of group $group_name
